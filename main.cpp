9ccd8526f5e74bcd7ff65cf3955c3ca980a63fcc
ManveerSingh0 <singhcoder2005@gmail.com>
2025-05-02 18:05:41 +0530

    building todo app

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// my defined libraries
+#include "todo.h"
+
+
+
+#include <QApplication>
+
+
+int main(int argc, char* argv[]) {
+  QApplication app(argc, argv);
+
+  Todo todo;
+  todo.show();
+
+
+  app.exec();
+}
---
+++ todo.h
@@ -0,0 +1,22 @@
+#ifndef TODO_H
+#define TODO_H
+
+
+// Qt libraries
+#include <QMainWindow>
+
+
+class Todo : public QMainWindow {
+
+  Q_OBJECT
+public:
+  Todo(QMainWindow* parent = nullptr);
+};
+
+
+
+
+
+
+
+#endif //TODO_H 
