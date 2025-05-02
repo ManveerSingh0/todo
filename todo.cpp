e8c5719926f9866112f9b95c01fe6d0d051b42ee
ManveerSingh0 <singhcoder2005@gmail.com>
2025-05-02 23:38:57 +0530

    actually learning something

--- todo.cpp
+++ todo.cpp
@@ -1,29 +1,11 @@
 #include "todo.h"
-#include <QListWidgetItem>
-#include <QWidget>
-#include <QVBoxLayout>
-#include <QListWidget>
-#include <qboxlayout.h>
-#include <qlistwidget.h>
-#include <qnamespace.h>
+
 
 
 Todo::Todo(QMainWindow* parent) : QMainWindow(parent) {
-  this->resize(400, 400);
-  this->setWindowTitle("To-Do");
-
-  QWidget *central_widget = new QWidget(this);
-  QVBoxLayout *layout = new QVBoxLayout(central_widget);
-
-
-  QListWidget* list_widget = new QListWidget(central_widget);
-  layout->addWidget(list_widget);
-
-  QListWidgetItem *task_item = new QListWidgetItem("Buy Groceries");
-  task_item->setFlags(task_item->flags() | Qt::ItemIsUserCheckable);
-  task_item->setCheckState(Qt::Unchecked);
-  list_widget->addItem(task_item);
-
-
-  this->setCentralWidget(central_widget);
+  
 }
+
+void Todo::add_task() {}
+void Todo::delete_task() {}
+void Todo::save_tasks(){}
--- todo.h
+++ todo.h
@@ -2,15 +2,30 @@
 #define TODO_H
 
 
+
 // Qt libraries
 #include <QMainWindow>
+#include <QListWidget>
+#include <QPushButton>
 
 
 class Todo : public QMainWindow {
 
   Q_OBJECT
 public:
-  Todo(QMainWindow* parent = nullptr);
+  Todo(QMainWindow *parent = nullptr);
+
+
+private slots:
+  void add_task();
+  void delete_task();
+  void save_tasks();
+
+private:
+  QListWidget* task_list;
+  QLineEdit* task_input;
+  QPushButton* add_button;
+  QPushButton* delete_button;
 };
 
 
